dcf0612bd9d76e03fd364ccc6df152dae6dc36d8
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-01-23 15:09:58 -0300

    Tunando o modelo Post no Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'slug', 'autor', 'publicado', 'status']
+    list_filter = ['status', 'criado', 'publicado', 'autor']
+    search_fields = ['titulo', 'corpo']
+    prepopulated_fields = {'slug': ('titulo',)}
+    raw_id_fields = ['autor']
+    date_hierarchy = 'publicado'
+    ordering = ['status', 'publicado']
