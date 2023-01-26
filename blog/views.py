6c20f604526c2821db0019323730b22ac7ecd781
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-01-26 16:55:26 -0300

    Página da lista de posts

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from .models import Post
 # Create your views here.
 def lista_de_posts(request):
     posts = Post.publicacoes.all()
-    return render(request, 'lista.html', {'posts': posts})
+    return render(request, 'blog/post/lista.html', {'posts': posts})
 
 def detalhe_do_post(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLICADO)
-    return render(request, 'detalhe.html', {'post': post})
+    return render(request, 'blog/post/detalhe.html', {'post': post})
--- setup/settings.py
+++ setup/settings.py
@@ -55,9 +55,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates',
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
