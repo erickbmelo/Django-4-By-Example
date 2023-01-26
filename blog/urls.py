d80a75ce511f5a1e9469b0748bc3f1c54316caaf
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-01-26 17:04:53 -0300

    Testando as urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     path('', views.lista_de_posts, name='lista_de_posts'),
-    path('<int:id>/', views.detalhe_do_post, name='detalhe_do_post')
+    path('post-<int:id>/', views.detalhe_do_post, name='detalhe_do_post')
 ]
