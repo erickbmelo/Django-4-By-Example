6f16942f9d0d682d930a600a637989a66f242784
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-01-30 18:22:30 -0300

    Criando páginação básica

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import get_object_or_404, render
-
 from .models import Post
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 def lista_de_posts(request):
-    posts = Post.publicacoes.all()
+    lista_posts = Post.publicacoes.all()
+    # Paginação com 3 posts por página
+    paginator = Paginator(lista_posts, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/lista.html', {'posts': posts})
 
 def detalhe_do_post(request, year, month, day, post):
