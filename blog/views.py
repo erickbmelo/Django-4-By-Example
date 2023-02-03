658ce287f967a5453c4f60f13236a111ac2e5783
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-02-03 17:32:10 -0300

    Fazer retornar a última página se a url requisitar além do range

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 # Create your views here.
 def lista_de_posts(request):
@@ -8,7 +8,11 @@ def lista_de_posts(request):
     # Paginação com 3 posts por página
     paginator = Paginator(lista_posts, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # Se for requisitada uma página maior que o range, devolve a última
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/lista.html', {'posts': posts})
 
 def detalhe_do_post(request, year, month, day, post):
